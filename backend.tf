34148e3ce2356d97513b0eb748fab69940651655
Dorinetk <dketchiozo@gmail.com>
2023-03-18 14:45:54 -0400

    updated bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-nk"
+    bucket = "terraform-backend-2023"
     key    = "cicd-lambda/terraform.tfstate"
     region = "us-east-1"
   }
